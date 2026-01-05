64817e9824a115795b565f0baa3838f182c2bf34
SyedMuqeem03 <Smuqeem03@gmail.com>
2026-01-06 00:51:02 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+
+def main():
+    print("Hello from Python app running in Jenkins!")
+    result = add(2, 3)
+    print(f"2 + 3 = {result}")
+
+
+if __name__ == "__main__":
+    main()
+
